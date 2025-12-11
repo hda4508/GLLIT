9ac560d65e2deb9957fbb3bc314f348c961c22ee
hda4508 <hda4508@gmail.com>
2025-12-11 23:25:42 +0900

    Render deploy fix: add dynamic port

--- app.js
+++ app.js
@@ -432,5 +432,5 @@ app.use("/mypage", mypageRoutes);
 // =======================================================
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
